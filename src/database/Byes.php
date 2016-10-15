68e4852cd63f3a9d3f879d3014107cb4fefd1680
LordKahra <root@localhost>
2016-10-15 15:19:40 -0400

    Updated Byes and Matches

--- src/database/Byes.php
+++ src/database/Byes.php
@@ -1,7 +1,25 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: chaos
- * Date: 10/15/2016
- * Time: 12:28
- */
+
+namespace kahra\src\database;
+
+use kahra\src\database\Object;
+
+class Bye extends Object {
+    //const FIELD_ID          = "id";
+    const FIELD_PARENT_ID   = "round_id";
+    const TABLE_NAME        = "byes";
+    const NAME_SINGULAR     = "bye";
+    const ALIAS             = "bye";
+    const FIELDS_SELECT     = "id,round_id,player_id";
+    const FIELDS_INSERT     = "round_id,player_id";
+
+    // CUSTOM QUERIES ////
+
+    static function getByRoundId($id) {
+        return static::getByField("round_id", $id);
+    }
+    
+    static function getByPlayerId($id) {
+        return static::getByField("player_id" $id);
+    }
+}
--- src/database/Matches.php
+++ src/database/Matches.php
@@ -1,7 +1,25 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: chaos
- * Date: 10/15/2016
- * Time: 12:28
- */
+
+namespace kahra\src\database;
+
+use kahra\src\database\Object;
+
+class Match extends Object {
+    //const FIELD_ID          = "id";
+    const FIELD_PARENT_ID   = "round_id";
+    const TABLE_NAME        = "matches";
+    const NAME_SINGULAR     = "match";
+    const ALIAS             = "match";
+    const FIELDS_SELECT     = "id,round_id,table";
+    const FIELDS_INSERT     = "round_id,table";
+
+    // CUSTOM QUERIES ////
+
+    static function getByRoundId($id) {
+        return static::getByField("round_id", $id);
+    }
+    
+    static function getByTable($table) {
+        return static::getByField("table" $id);
+    }
+}
