0c1f880ab9b455fa8a16e2a02aff4fe2563a1a60
LordKahra <lordkahra@gmail.com>
2016-10-27 16:21:14 -0400

    Index Require
    Changing the index require_once line.

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 
-require_once str_replace("//", "/", $_SERVER['DOCUMENT_ROOT'] . (((strpos($_SERVER['DOCUMENT_ROOT'], 'wamp') === false)) ? '' : '/tournament-api') . '/src/config/app_config.php');
-//require_once (getenv('root_pairings') . '/scripts/app_config.php');
+//require_once str_replace("//", "/", $_SERVER['DOCUMENT_ROOT'] . (((strpos($_SERVER['DOCUMENT_ROOT'], 'wamp') === false)) ? '' : '/tournament-api') . '/src/config/app_config.php');
+require_once (getenv("SITE_ROOT_API_TOURNAMENT") . '\src\config\app_config.php');
 
 use kahra\src\util\Debug;
 
