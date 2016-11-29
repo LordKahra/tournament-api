8210980da4502e617f1cb5396485de7dbcb4ff59
LordKahra <lordkahra@gmail.com>
2016-11-28 22:58:45 -0500

    Amazon S3

--- src/view/tournament/upload.php
+++ src/view/tournament/upload.php
@@ -10,6 +10,9 @@ use kahra\src\util\Validation;
 
 use kahra\src\view\APIResponse;
 
+echo APIResponse::getFailure(-1, "Debugging.");
+exit();
+
 $tournament = (array_key_exists("upload_data", $_POST)) ? $_POST["upload_data"] : false;
 
 // Get type specific information.
